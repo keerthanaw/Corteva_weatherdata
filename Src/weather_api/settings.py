e57afad49bbf41640157ad8c26a0dc8816ad682d
SK <keerthanaw@gmail.com>
2023-03-14 13:02:26 -0500

    Update settings.py

--- Src/weather_api/settings.py
+++ Src/weather_api/settings.py
@@ -7,8 +7,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@(9l(5tdnpiz!gh#@ct+alhmpvmccv(s)e@9ach8-fqa5xqxp#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
